e9d11e4edcb6f3dffa6ca04266401933b301cc4c
Daniel-Asare <johndoe@example.com>
2021-05-24 22:07:52 +0000

    Updates

---
+++ precache-manifest.4388077b94837028e6427872453cf928.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba590bd84740a1cd43c797517e6ec6fe",
+    "url": "/color-generator/index.html"
+  },
+  {
+    "revision": "262997fc8970e1641c7e",
+    "url": "/color-generator/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/color-generator/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/color-generator/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "262997fc8970e1641c7e",
+    "url": "/color-generator/static/js/main.ba25755c.chunk.js"
+  },
+  {
+    "revision": "ed92ef5652ce8f57a79c",
+    "url": "/color-generator/static/js/runtime-main.c4c3dd28.js"
+  }
+]);
